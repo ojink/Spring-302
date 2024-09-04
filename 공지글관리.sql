2f258b793df3174d3695db4369b0aa3d0b65c0fa
ojink <ojink2@naver.com>
2024-09-04 17:20:48 +0900

    답글컬럼추가, 답글관리

--- 공지글관리.sql
+++ 공지글관리.sql
@@ -8,8 +8,26 @@ writer        varchar2(50) constraint notice_writer_fk
 writedate     date default sysdate, /* 작성일자 */
 readcnt       number default 0, /* 조회수 */
 filepath      varchar2(300), /* 첨부파일경로 */
-filename      varchar2(300) /* 첨부파일명 */
+filename      varchar2(300), /* 첨부파일명 */
+rid           number, /* 원글id */
+root          number, /* 최초원글id */
+step          number default 0, /* 순서 */
+indent        number default 0, /* 들여쓰기 */
+constraint notice_rid_fk foreign key(rid) references notice(id) on delete cascade
 );
+
+-- 답글관련 컬럼 추가
+alter table notice add (
+rid           number, /* 원글id */
+root          number, /* 최초원글id */
+step          number default 0, /* 순서 */
+indent        number default 0, /* 들여쓰기 */
+constraint notice_rid_fk foreign key(rid) references notice(id) on delete cascade
+);
+
+select id, rid, root, step, indent from notice order by id desc;
+update notice set root = id;
+commit;
 
 create sequence seq_notice start with 1 increment by 1 nocache;
 
@@ -26,10 +44,37 @@ create or replace trigger trg_notice
     for each row
 begin
     select seq_notice.nextval into :new.id from dual;
+    
+    -- 원글인 경우만 root에 id와 같은 값(currval) 넣기
+    if( :new.root is null ) then
+        select seq_notice.currval into :new.root from dual;
+    else
+    -- 답글인 경우 순서를 위한  step변경
+        update notice set step = step + 1
+        where root = :new.root and step >= :new.step;
+    end if;
+    
 end;
 /
 
 
-select * from notice;
 
+
+select id, title from notice;
+
+select * from 
+(select row_number() over(order by id) no, name, n.* 
+from notice n left outer join member m on m.userid = n.writer) n 
+where no  between 375 and 384
+order by no desc;
+
+insert into notice ( title, content, writer )
+select title, content, writer from notice;
+commit;
+
+
+select id, filename, filepath from notice order by id desc;
+
+select count(*) from notice 
+where title like '%공%';
 
