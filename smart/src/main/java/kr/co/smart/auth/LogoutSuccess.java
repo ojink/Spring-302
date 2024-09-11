efa4974687e4ad2c0322e561bd93db42f223197a
ojink <ojink2@naver.com>
2024-09-11 14:41:09 +0900

    카카오로그아웃

--- smart/src/main/java/kr/co/smart/auth/LogoutSuccess.java
+++ smart/src/main/java/kr/co/smart/auth/LogoutSuccess.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -16,12 +17,23 @@ import lombok.RequiredArgsConstructor;
 @Component @RequiredArgsConstructor
 public class LogoutSuccess implements LogoutSuccessHandler{
 	private final CommonUtility common;
+	@Value("${kakao.client-id}") private String kakaoId;
 	
 	@Override
-	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response
+								, Authentication auth)
 			throws IOException, ServletException {
-		//로그인유지 해제 + DB 정보 삭제
-		response.sendRedirect( common.appURL(request) ); //웰컴페이지로 연결
+		
+		String redirect = null;
+		//소셜(카카오)로그인시 카카오계정도 함께 로그아웃 되도록 하기
+		if( "K".equals( ((LoginUser)auth.getPrincipal()).getUser().getSocial()) ) {
+			StringBuffer url = new StringBuffer("https://kauth.kakao.com/oauth/logout");
+			url.append("?client_id=").append(kakaoId);
+			url.append("&logout_redirect_uri=").append( common.appURL(request) );
+			redirect =  url.toString();
+		}else
+			redirect = common.appURL(request);
+		response.sendRedirect( redirect ); //웰컴페이지로 연결
 	}
 
 }
